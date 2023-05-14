965f4dcb003cd0d2e5af016d7d3f1b1562127334
Sergey A. Kozlov <65397484+Sergey371@users.noreply.github.com>
2023-05-14 10:49:29 +0300

    Update keymap.c
    Added KC_PSCR, KC_SCRL, KC_PAUS, KC_INS and KC_HOME to Fn layer as BlitzWolf BW-KB1.

--- keyboards/kprepublic/bm60hsrgb_iso/rev2/keymaps/default/keymap.c
+++ keyboards/kprepublic/bm60hsrgb_iso/rev2/keymaps/default/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_60_iso_arrow(
         QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, KC_PAUS, _______, _______, _______, _______,
-        _______, _______, _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_PSCR, KC_SCRL, KC_PAUS, _______, _______, _______, _______,
+        _______, _______, _______, RGB_HUD, RGB_SAD, RGB_VAD,  KC_INS, KC_HOME, KC_PGUP, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, KC_PGUP, _______,
         _______, _______, _______,                            _______,                   _______, _______, KC_HOME, KC_PGDN, KC_END
     )
